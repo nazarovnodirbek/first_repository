1f6eb71c8bc36f93d9dbac67725f516d7c809093
nazarovnodirbek <nazarov.nodirbek2007@gmail.com>
2026-03-09 13:24:05 +0500

    Add files via upload

---
+++ development_2.py
@@ -0,0 +1,24 @@
+
+#a=1
+#b=20
+#while a<b:
+  #  a+=1
+ #   if a%2==0:
+#        print(a)
+
+#a=2
+#while True: (agar cheksizlik bo'sa)
+#    a+=1
+#   print(a)
+ #   if a==100:
+ #       print('aaaaaaaaaaa')
+ #       break
+
+#a=str(input("isminggizni kiriting")
+#print(f'salom {a}')
+
+#a=str(input("isminggizni kiriting; "))
+#b=int(input("telefon raqam; "))
+#with open ("matn.txt", "w") as f:
+ #   f.write(f"ism; {a}\nTelefon raqam; {b}")
+
